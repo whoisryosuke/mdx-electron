c5c4031db2e544e5f370f4711cb81d98bb1ae4c6
Ryosuke <ryosuke.san.hana@gmail.com>
2021-01-18 12:39:05 -0800

    :bug: remove CSS hash name - breaks editor

--- configs/webpack.config.renderer.prod.babel.js
+++ configs/webpack.config.renderer.prod.babel.js
@@ -68,9 +68,9 @@ export default merge(baseConfig, {
           {
             loader: 'css-loader',
             options: {
-              modules: {
-                localIdentName: '[name]__[local]__[hash:base64:5]',
-              },
+              // modules: {
+              //   localIdentName: '[name]__[local]__[hash:base64:5]',
+              // },
               sourceMap: true,
             },
           },
