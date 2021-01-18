acdafde52a323f7fb6bc9e8d83a8ef7ad35099d0
Ryosuke <ryosuke.san.hana@gmail.com>
2021-01-18 12:39:38 -0800

    :zap: styles and save as Markdown

--- app/components/CodeEditor.tsx
+++ app/components/CodeEditor.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import fs from 'fs';
 import path from 'path';
-import { EditorState, ContentState } from 'draft-js';
+import { stateFromMarkdown } from 'draft-js-import-markdown';
+import draftToMarkdown from 'draftjs-to-markdown';
+import { EditorState, ContentState, convertToRaw } from 'draft-js';
 import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 
@@ -12,16 +14,18 @@ export const CodeEditor = (props: Props) => {
   const filePath = path.join(__dirname, './content/test.mdx');
   const onChange = (newValue) => {
     setCode(newValue);
-    console.log('edited text', newValue);
-    // fs.writeFileSync(filePath, newValue);
+    console.log('edited text', newValue.getCurrentContent());
+    const rawContentState = convertToRaw(newValue.getCurrentContent());
+    const markup = draftToMarkdown(rawContentState);
+    console.log('saving markdown', markup);
+    fs.writeFileSync(filePath, markup);
   };
 
   useEffect(() => {
     const mdxFile = fs.readFileSync(filePath, 'utf8');
-    setCode(
-      EditorState.createWithContent(ContentState.createFromText(mdxFile))
-    );
-  }, [setCode, filePath]);
+    setCode(EditorState.createWithContent(stateFromMarkdown(mdxFile)));
+    console.log('loading from file');
+  }, []);
   return (
     <Editor
       editorState={code}
