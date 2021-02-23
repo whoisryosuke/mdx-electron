fbf060ed662b886963d521f750a54e6fd178b8f9
Ryosuke <ryosuke.san.hana@gmail.com>
2021-02-22 22:32:50 -0800

    :sparkles: monaco editor

--- app/components/CodeEditor.tsx
+++ app/components/CodeEditor.tsx
@@ -1,62 +1,52 @@
 import React, { useEffect, useRef, useState } from 'react';
 import fs from 'fs';
 import path from 'path';
-import { draftToMarkdown, markdownToDraft } from 'markdown-draft-js';
-import { EditorState, convertToRaw, convertFromRaw } from 'draft-js';
-import createMarkdownShortcutsPlugin from 'draft-js-markdown-shortcuts-plugin';
-import createCodeEditorPlugin from 'draft-js-code-editor-plugin';
-import createSideToolbarPlugin from '@draft-js-plugins/side-toolbar';
-// import { Editor } from 'react-draft-wysiwyg';
-import Editor from '@draft-js-plugins/editor';
-// import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import '@draft-js-plugins/side-toolbar/lib/plugin.css';
+import Editor, { loader } from '@monaco-editor/react';
 import { useColorMode } from '@chakra-ui/react';
-import { AccordionButton } from './CodeEditorButtons/AccordionButton';
+import nightOwl from 'monaco-themes/themes/Night Owl.json';
 
-const markdownShortcutPlugin = createMarkdownShortcutsPlugin();
-const sideToolbarPlugin = createSideToolbarPlugin();
-const codeEditorPlugin = createSideToolbarPlugin();
-const plugins = [markdownShortcutPlugin, sideToolbarPlugin, codeEditorPlugin];
+function ensureFirstBackSlash(str) {
+  return str.length > 0 && str.charAt(0) !== '/' ? `/${str}` : str;
+}
+
+function uriFromPath(_path) {
+  const pathName = path.resolve(_path).replace(/\\/g, '/');
+  return encodeURI(`file://${ensureFirstBackSlash(pathName)}`);
+}
+
+loader.config({
+  paths: {
+    vs: uriFromPath(
+      path.join(__dirname, '../node_modules/monaco-editor/min/vs')
+    ),
+  },
+});
 
 interface Props {
   filename: string;
   refreshPreview: () => void;
 }
 
-const toolbarOptions = {
-  options: [
-    'inline',
-    'blockType',
-    // 'fontSize',
-    // 'fontFamily',
-    'list',
-    // 'textAlign',
-    // 'colorPicker',
-    'link',
-    'embedded',
-    'emoji',
-    'image',
-    'remove',
-    'history',
-  ],
-};
-
 export const CodeEditor = React.memo(function CodeEditor({
   filename,
   refreshPreview,
 }: Props) {
-  const [code, setCode] = useState(EditorState.createEmpty());
-  const { colorMode, toggleColorMode } = useColorMode();
+  const [code, setCode] = useState('# Hello MDX!');
+  // const { colorMode, toggleColorMode } = useColorMode();
   const filePath = path.join(__dirname, `./content/${filename}`);
   console.log('filePath', filePath);
   const loadedFile = useRef('');
   const onChange = (newValue) => {
     setCode(newValue);
-    const rawContentState = convertToRaw(newValue.getCurrentContent());
-    const markdownChanges = draftToMarkdown(rawContentState);
-    fs.writeFileSync(filePath, markdownChanges);
+    fs.writeFileSync(filePath, newValue);
     refreshPreview();
   };
+
+  function handleEditorWillMount(monaco) {
+    // here is the monaco instance
+    // do something before editor is mounted
+    monaco.editor.defineTheme('night-owl', nightOwl);
+  }
 
   useEffect(() => {
     if (filename && filename !== '' && loadedFile.current !== filename) {
@@ -66,46 +56,23 @@ export const CodeEditor = React.memo(function CodeEditor({
       } catch (e) {
         console.error('failed to load file', e);
       } finally {
-        const contentState = convertFromRaw(markdownToDraft(mdxFile));
-        setCode(EditorState.createWithContent(contentState));
+        setCode(mdxFile);
         loadedFile.current = filename;
       }
     }
   }, [filename, filePath]);
 
-  const editorStyles =
-    colorMode === 'dark'
-      ? {
-          fontColor: 'rgba(255, 255, 255, 0.92)',
-        }
-      : {};
-  const toolbarStyles =
-    colorMode === 'dark'
-      ? {
-          backgroundColor: '#1A202C',
-          borderColor: '#1A202C',
-        }
-      : {};
+  console.log('the code', code);
+
   return (
     <Editor
-      editorState={code}
+      height="90vh"
+      defaultLanguage="markdown"
+      theme="night-owl"
+      value={code}
+      line={0}
       onChange={onChange}
-      plugins={plugins}
-      // toolbarClassName="MDXCodeEditor_Toolbar"
-      // wrapperClassName="MDXCodeEditor_Wrapper"
-      // editorClassName="MDXCodeEditor"
-      // toolbar={toolbarOptions}
-      // toolbarCustomButtons={[<AccordionButton key="accordion" />]}
-      // wrapperStyle={{
-      //   position: 'relative',
-      //   height: '100vh',
-      // }}
-      // editorStyle={{
-      //   overflow: 'none !important',
-      //   padding: '0 2em',
-      //   ...editorStyles,
-      // }}
-      // toolbarStyle={toolbarStyles}
+      beforeMount={handleEditorWillMount}
     />
   );
 });
