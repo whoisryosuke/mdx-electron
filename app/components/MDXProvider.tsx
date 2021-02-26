31a60ba4a4595d750a04ccd868a98dec30aaf99a
Ryosuke <ryosuke.san.hana@gmail.com>
2021-02-25 22:13:24 -0800

    :zap: syntax highlighting

--- app/components/MDXProvider.tsx
+++ app/components/MDXProvider.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable react/display-name */
 /* eslint-disable import/prefer-default-export */
 import React from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { darcula } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import {
   Alert,
   AlertIcon,
@@ -110,21 +112,32 @@ const ChakraUIComponents = {
 };
 
 export const UIComponents = {
-  h1: (props) => <Heading as="h1" size="2xl" {...props} />,
-  h2: (props) => <Heading as="h2" size="xl" {...props} />,
-  h3: (props) => <Heading as="h3" size="lg" {...props} />,
-  h4: (props) => <Heading as="h4" size="md" {...props} />,
-  h5: (props) => <Heading as="h5" size="sm" {...props} />,
-  h6: (props) => <Heading as="h6" size="xs" {...props} />,
-  p: (props) => <Text as="p" {...props} />,
-  hr: (props) => <Divider {...props} />,
-  ul: (props) => <UnorderedList {...props} />,
-  ol: (props) => <OrderedList {...props} />,
-  li: (props) => <ListItem {...props} />,
+  h1: (props) => <Heading as="h1" size="2xl" mb={3} {...props} />,
+  h2: (props) => <Heading as="h2" size="xl" mb={3} {...props} />,
+  h3: (props) => <Heading as="h3" size="lg" mb={3} {...props} />,
+  h4: (props) => <Heading as="h4" size="md" mb={3} {...props} />,
+  h5: (props) => <Heading as="h5" size="sm" mb={3} {...props} />,
+  h6: (props) => <Heading as="h6" size="xs" mb={3} {...props} />,
+  p: (props) => <Text as="p" mb={3} {...props} />,
+  hr: (props) => <Divider mb={3} {...props} />,
+  ul: (props) => <UnorderedList mb={3} {...props} />,
+  ol: (props) => <OrderedList mb={3} {...props} />,
+  li: (props) => <ListItem mb={3} {...props} />,
   pre: (props) => <div {...props} />,
-  code: (props) => <Code {...props} />,
+  code: (props) => (
+    <SyntaxHighlighter
+      mb={3}
+      language={
+        props.className
+          ? props.className.replace('language-', '')
+          : 'javascript'
+      }
+      style={darcula}
+      {...props}
+    />
+  ),
   blockquote: ({ children, ...restProps }) => (
-    <Alert status="info" {...restProps}>
+    <Alert status="info" mb={3} {...restProps}>
       <AlertIcon />
       {children}
     </Alert>
